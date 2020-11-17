e370df6500e60c84dd870c463321da2debc1c156
saiteja-code <saitejakanaparthy@gmail.com>
2020-11-17 18:43:16 +0530

    UC5

--- Email.js
+++ Email.js
@@ -8,7 +8,7 @@ class Email{
         return this._mail;
     }
     set mail(mail){
-        let partOneRegex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.][a-z]+$')
+        let partOneRegex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
         if(partOneRegex.test(mail)){
             this._mail = mail;
         }
@@ -20,10 +20,11 @@ class Email{
 try{
     let email1 = new Email("sai@gmail1.com")
     console.log(email1)
-    let email2 = new Email("sai.111@gmail.com")
+    let email2 = new Email("sai.111@gmail.com.co")
     console.log(email2)
-    let email3 = new Email("sai.@gmail.")
-    console.log(email3)
+   
+    let email5 = new Email("abc@gmail.com.aa.au")
+    console.log(email5)
 }catch(e){
     console.log("Error :  " + e)
 }
