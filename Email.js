d06108f3d1e3bef627843178ac38eb7b872af394
saiteja-code <saitejakanaparthy@gmail.com>
2020-11-17 18:35:34 +0530

    optional

--- Email.js
+++ Email.js
@@ -8,7 +8,7 @@ class Email{
         return this._mail;
     }
     set mail(mail){
-        let partOneRegex = RegExp('^[a-z]+[@][a-z0-9]+$')
+        let partOneRegex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.][a-z]+$')
         if(partOneRegex.test(mail)){
             this._mail = mail;
         }
@@ -18,10 +18,12 @@ class Email{
 
 
 try{
-    let email1 = new Email("Saiteja@gmail1")
+    let email1 = new Email("sai@gmail1.com")
     console.log(email1)
-    let email2 = new Email("Sai)
+    let email2 = new Email("sai.111@gmail.com")
     console.log(email2)
+    let email3 = new Email("sai.@gmail.")
+    console.log(email3)
 }catch(e){
     console.log("Error :  " + e)
 }
